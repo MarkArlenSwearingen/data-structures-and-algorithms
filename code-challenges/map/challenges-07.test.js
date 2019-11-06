e50bda1fac10756b77df3e2fffb9d0c04d0194d1
Mark Swearingen <markswearingen@bendcable.com>
2019-11-05 21:43:25 -0800

    3 passes

--- code-challenges/map/challenges-07.test.js
+++ code-challenges/map/challenges-07.test.js
@@ -38,6 +38,7 @@ Write a function named mapTwoToThe that produces the same output as your forLoop
 
 const mapTwoToThe = (arr) => {
   // Solution code here...
+  return arr.map(x => Math.pow(2, x));
 };
 
 /* ------------------------------------------------------------------------------------------------
