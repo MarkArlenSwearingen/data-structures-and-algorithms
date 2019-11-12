52e2a712f51b90479c0885bce3d6e1ea11a03632
Mark Swearingen <markswearingen@bendcable.com>
2019-11-12 14:59:03 -0800

    add challenge 1 to challenge 11

--- code-challenges/regex2/challenges-11.test.js
+++ code-challenges/regex2/challenges-11.test.js
@@ -10,7 +10,8 @@ If the PIN is four numerical digits long, return true. Otherwise, return false.
 
 const validatePin = (pin) => {
   // Solution code here...
-
+  let regex = /^[0-9][0-9][0-9][0-9]\b/gm;
+  pin.match(regex);
 };
 
 /* ------------------------------------------------------------------------------------------------
